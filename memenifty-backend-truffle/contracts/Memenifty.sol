bc86779764d8a7d9ae3d95d42d275c4ff96921d2
Logan Delafosse <logandelafosse@Logans-MacBook-Pro.local>
2021-07-12 16:11:11 -0500

    Fix compile issue, update solc in truffle-config

--- memenifty-backend-truffle/contracts/Memenifty.sol
+++ memenifty-backend-truffle/contracts/Memenifty.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract Memenifty is ERC721PresetMinterPauserAutoId {
 
-  constructor() public Memenifty("Memenifty", "MEME", "127.0.0.1:7545") {}
+  constructor() public ERC721PresetMinterPauserAutoId("Memenifty", "MEME", "127.0.0.1:7545") {}
 
 
 
