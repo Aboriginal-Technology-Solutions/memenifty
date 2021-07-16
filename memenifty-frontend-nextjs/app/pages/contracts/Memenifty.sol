cf09b0cfc09503f3cbb5e55e7db905480c357f47
Logan Delafosse <bioready@gmail.com>
2021-07-16 15:27:27 -0500

    update memenifty.sol

--- memenifty-frontend-nextjs/app/pages/contracts/Memenifty.sol
+++ memenifty-frontend-nextjs/app/pages/contracts/Memenifty.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Memenifty is ERC721 {
 
-  constructor() public ERC721PresetMinterPauserAutoId("MintedMeme", "MEME", "127.0.0.1:7545") {}
+  constructor() public ERC721("MintedMeme", "MEME") {}
 
 
 
