04b14d2d1cd56d354e0944ddabb2db1b8ea041d0
Logan Delafosse <bioready@gmail.com>
2021-07-16 15:27:04 -0500

    Update memenifty.sol

--- memenifty-frontend-nextjs/app/pages/contracts/Memenifty.sol
+++ memenifty-frontend-nextjs/app/pages/contracts/Memenifty.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "memenifty-frontend-nextjs/app/node_modules/@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Memenifty is ERC721PresetMinterPauserAutoId.sol {
+contract Memenifty is ERC721 {
 
   constructor() public ERC721PresetMinterPauserAutoId("MintedMeme", "MEME", "127.0.0.1:7545") {}
 
