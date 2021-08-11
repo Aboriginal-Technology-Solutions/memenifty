b0ad3295507db018c654ea01276a91b623ec0e55
Logan Delafosse <bioready@gmail.com>
2021-08-11 09:36:12 -0500

    Learn: use Bootstrap modals

--- memenifty-frontend-nextjs/memenifty/pages/Components/mainbody.tsx
+++ memenifty-frontend-nextjs/memenifty/pages/Components/mainbody.tsx
@@ -5,21 +5,48 @@ class MainBody extends React.Component{
   render() {
     return(
       <div className="container-fluid mt-5 mx-auto">
-      <br />
-      <br />
+        <br />
+        <br />
+
         <div className="d-flex mx-auto my-auto justify-content-center">
           <div className={styles.code}>
             What is Memenifty?
           </div>
         </div>
+
         <div className="d-flex mx-auto my-auto">
-          <p className="d-flex mx-5 my-auto ">
+          <p className="d-flex mx-auto my-auto">
             Memenifty is a project brainstormed by r/ethdev to help the community learn and grow together.
             This project will allow users to mint an image as an NFT to be stored on the blockchain forever.
           </p>
         </div>
-      </div>
 
+        <div className="d-flex justify-content-center mx-auto my-5">
+          <button type="button"
+                  class="btn btn-secondary"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal">
+          Mint a Meme!
+          </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
     )
   }
